aac97b2dcd55c1eeaf30c9cb64528c5ac396719d
Andres <andres7293@gmail.com>
2022-07-16 00:44:18 +0100

    [test] Creates a unit test using the mock server

--- test/proxy.test.js
+++ test/proxy.test.js
@@ -1,8 +1,12 @@
 const { proxyServer } = require('../src/app');
+const { MockServer } = require('./MockServer');
 
 const axios = require('axios').default;
 
+const mockServer = new MockServer();
+
 const PROXY_PORT       = 45667;
+const MOCK_SERVER_PORT = PROXY_PORT - 1;
 
 async function request_through_proxy(url) {
     return await axios.get(url, { proxy: 
@@ -16,10 +20,12 @@ async function request_through_proxy(url) {
 
 beforeAll(() => {
     proxyServer.listen(PROXY_PORT);
+    mockServer.listen(MOCK_SERVER_PORT);
 });
 
 afterAll(() => {
     proxyServer.close();
+    mockServer.close();
 });
 
 /*
@@ -36,4 +42,15 @@ test('GET / http://google.com/', async () => {
 test('GET / https://google.com', async () => {
     const response = await request_through_proxy(`http://google.com/`);
     expect(response.status).toBe(200);
+});
+
+test('GET / MockServer', async () => {
+    const mock_response = 'Hello';
+    const mock_status_code = 200;
+    mockServer.setResponse(mock_response);
+    mockServer.setStatusCode(mock_status_code);
+
+    const response = await request_through_proxy(`http://localhost:${MOCK_SERVER_PORT}`);
+    expect(response.status).toBe(mock_status_code);
+    expect(response.data).toBe(mock_response);
 });
